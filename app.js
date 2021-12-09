8e8ae3c6a506a185da8a6241d344a1a766d49d5b
muskanbatra-dev <muskanbatra64@gmail.com>
2021-12-10 01:49:25 +0530

    update

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-const billAmount = document.querySelector("#bill-amount"); 
+const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll("#no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
+billAmount.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
+cashGiven.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
+const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1]
+
+function showDivCash() {
+    document.getElementById("hideDivCash").style.display = "block";
+ }
+
+ function showDivChange() {
+    document.getElementById("hideDivChange").style.display = "block";
+ }
+
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    var billAm = Number(billAmount.value);
+    var cashGi = Number(cashGiven.value);
     hideMessgae();
-    if (billAmount.value > 0) {
-        if( cashGiven.value >= billAmount.value ) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+    if (billAm > 0) {
+        if (cashGi >= billAm) {
+            const amountToBeReturned = cashGi - billAm;
             calculateChange(amountToBeReturned);
         } else {
-            showMessage("The cash provided should atleast be qual to the bill amount");
+            showMessage("The cash provided should be equal or more than the bill amount");
         }
     } else {
         showMessage("Invalid Bill Amount")
@@ -22,17 +49,19 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 
 function calculateChange(amountToBeReturned) {
     for (let i = 0; i < availableNotes.length; i++) {
-        const numberOfNotes = math.trunc(amountToBeReturned / availableNotes[0]);
-
+        const numberOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+        );
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
     }
-}
+};
 
 function hideMessgae() {
     message.style.display = "none";
-}
+};
 
 function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
-}
-
+};
